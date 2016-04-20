d0900b5755ea71f1b3bbd50aa5df5fab97ebc975
Max Team <royalrmsiskd@gmail.com>
2016-04-20 04:03:30 -0700

    Update bot.lua

--- bot.lua
+++ bot.lua
@@ -6,8 +6,9 @@ JSON = require('dkjson')
 HTTPS = require('ssl.https')
 dofile('utilities.lua')
 ----config----
-local bot_api_key = "" --BOT TOKEN تو کن ربات خود را در اینجا قرار دهید
-local You = 188548712  --ID ADMIN ایدی خود را اینجا قرار دهید
+local bot_api_key = "203639501:AAEzVVgcb8akJeNQUGC6GHpIB74gKcsSORQ
+" --BOT TOKEN تو کن ربات خود را در اینجا قرار دهید
+local You = 170750859  --ID ADMIN ایدی خود را اینجا قرار دهید
 local BASE_URL = "https://api.telegram.org/bot"..bot_api_key
 local BASE_FOLDER = ""
 local start = [[ ]]
